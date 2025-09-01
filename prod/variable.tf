2913402c3fa8f84da981c8e34f9c1a85832e72a4
simonangel-fong <simonangelfong@gmail.com>
2025-09-01 15:14:09 -0400

    update github action

--- prod/variable.tf
+++ prod/variable.tf
@@ -7,8 +7,6 @@ variable "env" { default = "dev" }
 variable "aws_region" { type = string }
 variable "dns_domain" { default = "test-api.arguswatcher.net" }
 
-# variable "vpc_cidr" { type = string }
-
 # ##############################
 # S3 bucket
 # ##############################
@@ -19,47 +17,6 @@ variable "web_path" { default = "../web" }
 # DynamoDB Table
 # ##############################
 variable "csv_prefix" { default = "data" }
-
-
-# locals {
-#   dynamodb_tb = [
-#     {
-#       tb_name        = "mv_bike_count"
-#       hash_attr      = "pk"
-#       hash_attr_type = "S"
-#       csv_bucket_id  = "trip.arguswatcher.net"
-#       csv_key        = "data/mv_bike_count.csv"
-#     },
-#     {
-#       tb_name        = "mv_station_count"
-#       hash_attr      = "pk"
-#       hash_attr_type = "S"
-#       csv_bucket_id  = "trip.arguswatcher.net"
-#       csv_key        = "data/mv_station_count.csv"
-#     },
-#     {
-#       tb_name        = "mv_user_year_hour_trip"
-#       hash_attr      = "pk"
-#       hash_attr_type = "S"
-#       csv_bucket_id  = "trip.arguswatcher.net"
-#       csv_key        = "data/mv_user_year_hour_trip.csv"
-#     },
-#     {
-#       tb_name        = "mv_user_year_month_trip"
-#       hash_attr      = "pk"
-#       hash_attr_type = "S"
-#       csv_bucket_id  = "trip.arguswatcher.net"
-#       csv_key        = "data/mv_user_year_month_trip.csv"
-#     },
-#     {
-#       tb_name        = "mv_user_year_station"
-#       hash_attr      = "pk"
-#       hash_attr_type = "S"
-#       csv_bucket_id  = "trip.arguswatcher.net"
-#       csv_key        = "data/mv_user_year_station.csv"
-#     }
-#   ]
-# }
 
 # ##############################
 # API Gateway
