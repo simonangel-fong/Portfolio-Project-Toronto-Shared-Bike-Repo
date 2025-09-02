dda11bd645cab774ee4b69de4ca3f23cfc8e0256
simonangel-fong <simonangelfong@gmail.com>
2025-09-02 15:54:40 -0400

    add locust

---
+++ locust/locustfile.py
@@ -0,0 +1,37 @@
+from locust import HttpUser, task, between
+
+API_URLS = {
+    "home": "https://trip.arguswatcher.net/",
+    "bike": "https://trip.arguswatcher.net/prod/bike",
+    "station": "https://trip.arguswatcher.net/prod/station",
+    "top_station": "https://trip.arguswatcher.net/prod/top-station",
+    "trip_month": "https://trip.arguswatcher.net/prod/trip-month",
+    "trip_hour": "https://trip.arguswatcher.net/prod/trip-hour",
+}
+
+class WebsiteUser(HttpUser):
+    wait_time = between(5, 9)
+
+    @task
+    def home(self):
+        self.client.get(API_URLS["home"])
+
+    @task
+    def bike(self):
+        self.client.get(API_URLS["bike"])
+
+    @task
+    def station(self):
+        self.client.get(API_URLS["station"])
+
+    @task
+    def top_station(self):
+        self.client.get(API_URLS["top_station"])
+
+    @task
+    def trip_month(self):
+        self.client.get(API_URLS["trip_month"])
+
+    @task
+    def trip_hour(self):
+        self.client.get(API_URLS["trip_hour"])
