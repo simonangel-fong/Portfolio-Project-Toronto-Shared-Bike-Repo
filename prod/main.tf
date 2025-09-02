4da4001bbc882eacf534f300f94a3dd9a5eda046
simonangel-fong <simonangelfong@gmail.com>
2025-09-01 20:52:09 -0400

    update tf

--- prod/main.tf
+++ prod/main.tf
@@ -38,50 +38,50 @@ module "lambda" {
   dynamodb_table_arn  = module.dynamodb_tb.arn
 }
 
-# # ##############################
-# # AWS API Gateway
-# # ##############################
-# module "api_gateway" {
-#   source  = "../module/apigw"
-#   project = var.project
-#   app     = var.app
-#   env     = var.env
+# ##############################
+# AWS API Gateway
+# ##############################
+module "api_gateway" {
+  source  = "../module/apigw"
+  project = var.project
+  app     = var.app
+  env     = var.env
 
-#   path_list  = var.path_list
-#   lambda_arn = module.lambda.arn
-#   lambda_id  = module.lambda.id
-# }
+  path_list  = var.path_list
+  lambda_arn = module.lambda.arn
+  lambda_id  = module.lambda.id
+}
 
-# # ##############################
-# # AWS Cloudfront
-# # ##############################
-# module "cloudfront" {
-#   source  = "../module/cloudfront"
-#   project = var.project
-#   app     = var.app
-#   env     = var.env
+# ##############################
+# AWS Cloudfront
+# ##############################
+module "cloudfront" {
+  source  = "../module/cloudfront"
+  project = var.project
+  app     = var.app
+  env     = var.env
 
-#   # domain
-#   dns_domain  = var.dns_domain
-#   cert_domain = var.cert_domain
-#   # api
-#   apigw_stage = module.api_gateway.stage
-#   apigw_id    = module.api_gateway.id
-#   # s3 web
-#   website_endpoint = module.csv_bucket.website_endpoint
-# }
+  # domain
+  dns_domain  = var.dns_domain
+  cert_domain = var.cert_domain
+  # api
+  apigw_stage = module.api_gateway.stage
+  apigw_id    = module.api_gateway.id
+  # s3 web
+  website_endpoint = module.csv_bucket.website_endpoint
+}
 
-# # ##############################
-# # Cloudflare DNS
-# # ##############################
-# module "cloudflare_dns" {
-#   source  = "../module/dns"
-#   project = var.project
-#   app     = var.app
-#   env     = var.env
-#   # cloudflare config
-#   cloudflare_zone_id   = var.cloudflare_zone_id
-#   cloudflare_api_token = var.cloudflare_api_token
-#   dns_domain           = var.dns_domain
-#   target_domain        = module.cloudfront.domain
-# }
+# ##############################
+# Cloudflare DNS
+# ##############################
+module "cloudflare_dns" {
+  source  = "../module/dns"
+  project = var.project
+  app     = var.app
+  env     = var.env
+  # cloudflare config
+  cloudflare_zone_id   = var.cloudflare_zone_id
+  cloudflare_api_token = var.cloudflare_api_token
+  dns_domain           = var.dns_domain
+  target_domain        = module.cloudfront.domain
+}
