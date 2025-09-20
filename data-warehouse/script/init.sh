9a791ccac66fa07730cd9d0c3b92fb033d42b6d6
simonangel-fong <simonangelfong@gmail.com>
2025-09-20 16:22:54 -0400

    update init.sh

--- data-warehouse/script/init.sh
+++ data-warehouse/script/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JENKINS_DOCKER_FILE="../docker-compose.yaml"
+JENKINS_DOCKER_FILE="../jenkins/docker-compose.yaml"
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
