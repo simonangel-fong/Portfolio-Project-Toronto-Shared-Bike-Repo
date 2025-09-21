7793abad08f6f04889e254073cb185302d4482ee
simonangel-fong <simonangelfong@gmail.com>
2025-09-20 23:09:49 -0400

    test jenkinsfile

--- data-warehouse/script/init.sh
+++ data-warehouse/script/init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-JENKINS_FILE="data-warehouse/jenkins/docker-compose.yaml"
 PROMETHEUS_FILE="data-warehouse/monitor/docker-compose.yaml"
 
 echo
@@ -50,12 +49,25 @@ docker run hello-world
 
 echo
 echo "##############################"
-echo "Start Jenkins"
+echo "Install Jenkins"
 echo "##############################"
 echo
-# spin up Jenkins
-docker compose -f $JENKINS_FILE down
-docker compose -f $JENKINS_FILE up -d --build
+# Installation of Java
+sudo apt update
+sudo apt install -y fontconfig openjdk-21-jre
+java -version
+openjdk version "21.0.3" 2024-04-16
+OpenJDK Runtime Environment (build 21.0.3+11-Debian-2)
+OpenJDK 64-Bit Server VM (build 21.0.3+11-Debian-2, mixed mode, sharing)
+
+# Install Jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install -y jenkins
 
 echo
 echo "##############################"
