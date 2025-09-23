ce33eef8cf332ff6095e226381da79937816a43f
simonangel-fong <simonangelfong@gmail.com>
2025-09-23 01:00:54 -0400

    test jenkinsfile

--- data-warehouse/script/get_raw.sh
+++ data-warehouse/script/get_raw.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 DATA_DIR="/data"
 ZIP_FILE="csv.zip"
 
-
 echo
 echo "##############################"
 echo "Download csv zip"
@@ -13,7 +12,6 @@ echo "##############################"
 echo
 sudo mkdir -pv $DATA_DIR
 sudo curl -o $DATA_DIR/$ZIP_FILE https://toronto-shared-bike-data-warehouse-data-bucket.s3.ca-central-1.amazonaws.com/raw/data.zip
-sudo curl -o /data/csv.zip https://toronto-shared-bike-data-warehouse-data-bucket.s3.ca-central-1.amazonaws.com/raw/data.zip
 
 echo
 echo "##############################"
--- data-warehouse/script/init.sh
+++ data-warehouse/script/init.sh
@@ -47,6 +47,10 @@ git clone https://github.com/simonangel-fong/Portfolio-Project-Toronto-Shared-Bi
 cd ~/project-shared-bike
 git checkout feature/dw
 bash ~/project-shared-bike/data-warehouse/script/install.sh
+bash ~/project-shared-bike/data-warehouse/script/get_raw.sh
+
+sudo mkdir -pv /export
+sudo chown 999:999 /export
 
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 # unzip awscliv2.zip
