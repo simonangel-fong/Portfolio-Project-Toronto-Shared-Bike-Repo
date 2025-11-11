9ab006d9b2b7d0aea42e660be16f3bc908a5960c
simonangel-fong <simonangelfong@gmail.com>
2025-11-10 21:54:36 -0500

    test cicd

--- testing/load/cloud_load.js
+++ testing/load/cloud_load.js
@@ -1,18 +1,23 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-const DNS_DOMAIN = __ENV.DNS_DOMAIN;
-const API_ENV = __ENV.API_ENV;
+const TEST = __ENV.TEST || "Smoke test";
+const ENV = __ENV.ENV || "dev";
 
-const HOME_URL = `https://${DNS_DOMAIN}`;
-const BIKE_URL = `https://${DNS_DOMAIN}/${API_ENV}/bike`;
-const STATION_URL = `https://${DNS_DOMAIN}/${API_ENV}/station`;
-const TRIP_HOUR_URL = `https://${DNS_DOMAIN}/${API_ENV}/trip-hour`;
-const TRIP_MONTH_URL = `https://${DNS_DOMAIN}/${API_ENV}/trip-month`;
-const TOP_STATION_URL = `https://${DNS_DOMAIN}/${API_ENV}/top-station`;
+const DOMAIN = __ENV.DOMAIN || "localhost";
+const HOME_URL = `https://${DOMAIN}`;
+const BIKE_URL = `https://${DOMAIN}/${ENV}/bike`;
+const STATION_URL = `https://${DOMAIN}/${ENV}/station`;
+const TRIP_HOUR_URL = `https://${DOMAIN}/${ENV}/trip-hour`;
+const TRIP_MONTH_URL = `https://${DOMAIN}/${ENV}/trip-month`;
+const TOP_STATION_URL = `https://${DOMAIN}/${ENV}/top-station`;
 
-const SLA_FAIL = __ENV.SLA_FAIL || 0.01;
-const SLA_DUR_99 = __ENV.SLA_DUR_99 || 1000;
+const VU = __ENV.VU || 5;
+const SCALE = __ENV.SCALE || 1;
+const DURATION = __ENV.DURATION || "30s";
+
+const SLA_FAIL = __ENV.SLA_FAIL || "0.01";
+const SLA_DUR_99 = __ENV.SLA_DUR_99 || "1000";
 
 const TARGET = 30;
 
@@ -25,42 +30,43 @@ export const options = {
     average_load: {
       executor: "ramping-vus",
       stages: [
-        { duration: "1m", target: TARGET },
-        { duration: "1m", target: TARGET },
-        { duration: "1m", target: 0 },
+        { duration: DURATION, target: VU },
+        { duration: DURATION, target: VU },
+        { duration: DURATION, target: 0 },
       ],
     },
   },
   cloud: {
-    name: "API Load Testing",
+    name: TEST,
   },
 };
 
 // Smoke testing
 export default () => {
-  // home
-  const homeRes = http.get(HOME_URL);
-  check(homeRes, { "status returned 200": (r) => r.status == 200 });
+  for (let i = 0; i < SCALE; i++) {
+    // home
+    const homeRes = http.get(HOME_URL);
+    check(homeRes, { "status returned 200": (r) => r.status == 200 });
 
-  // bike
-  const bikeRes = http.get(BIKE_URL);
-  check(bikeRes, { "status returned 200": (r) => r.status == 200 });
+    // bike
+    const bikeRes = http.get(BIKE_URL);
+    check(bikeRes, { "status returned 200": (r) => r.status == 200 });
 
-  // station
-  const stationRes = http.get(STATION_URL);
-  check(stationRes, { "status returned 200": (r) => r.status == 200 });
+    // station
+    const stationRes = http.get(STATION_URL);
+    check(stationRes, { "status returned 200": (r) => r.status == 200 });
 
-  // trip hour
-  const tripHourRes = http.get(TRIP_HOUR_URL);
-  check(tripHourRes, { "status returned 200": (r) => r.status == 200 });
+    // trip hour
+    const tripHourRes = http.get(TRIP_HOUR_URL);
+    check(tripHourRes, { "status returned 200": (r) => r.status == 200 });
 
-  // trip month
-  const tripMonthRes = http.get(TRIP_MONTH_URL);
-  check(tripMonthRes, { "status returned 200": (r) => r.status == 200 });
+    // trip month
+    const tripMonthRes = http.get(TRIP_MONTH_URL);
+    check(tripMonthRes, { "status returned 200": (r) => r.status == 200 });
 
-  // top station
-  const topStationRes = http.get(TOP_STATION_URL);
-  check(topStationRes, { "status returned 200": (r) => r.status == 200 });
-
+    // top station
+    const topStationRes = http.get(TOP_STATION_URL);
+    check(topStationRes, { "status returned 200": (r) => r.status == 200 });
+  }
   sleep(1);
 };
